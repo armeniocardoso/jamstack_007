ffe841e88d5549313d3426b77f5bd5458d064a75
armeniocardoso <armeniotorres@gmail.com>
2023-10-31 17:31:28 -0300

    Blog Pronto

--- src/pages/blog/index.js
+++ src/pages/blog/index.js
@@ -1,6 +1,6 @@
 import * as React from 'react'
-import { useStaticQuery, graphql } from 'gatsby'
-import Layout from '../components/layout'
+import { Link, useStaticQuery, graphql } from 'gatsby'
+import Layout from '../../components/layout'
 
 const BlogPage = () => {
 
@@ -11,6 +11,7 @@ const BlogPage = () => {
           frontmatter {
             date(formatString: "MMMM D, YYYY")
             title
+            slug
           }
           id
           excerpt
@@ -23,11 +24,14 @@ const BlogPage = () => {
     <Layout>
       <p>Esses são os últimos posts:</p>
       {
-        data.allMdx.nodes.map((node) => (
+        data.allMdx.nodes.map(node => (
           <article key={node.id}>
-            <h2>{node.frontmatter.title}</h2>
+            <h2>
+              <Link to={`/blog/${node.frontmatter.slug}`}>
+                {node.frontmatter.title}
+              </Link>
+            </h2>
             <p>Posted: {node.frontmatter.date}</p>
-            <p>{node.excerpt}</p>
           </article>
         ))
       }
